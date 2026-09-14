215ed754c6b87fad86e8e914585041a6a75af616
KENNETH-ARYATUHA <191531526+KENNETH-ARYATUHA@users.noreply.github.com>
2026-09-14 17:14:03 +0300

    Remove HelmetProvider from main.jsx
    Removed HelmetProvider wrapping from App component.

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,11 @@
 // Entry point: mounts the <App /> component into the #root div in index.html
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelmetProvider>
-      <App />
-    </HelmetProvider>
+    <App />
   </React.StrictMode>
 );
