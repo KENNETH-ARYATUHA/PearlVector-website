b1f135ee6ecdbe603e68de4bda51f4cf5924efb6
KENNETH-ARYATUHA <191531526+KENNETH-ARYATUHA@users.noreply.github.com>
2026-09-14 16:10:09 +0300

    Wrap App component with HelmetProvider

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
 // Entry point: mounts the <App /> component into the #root div in index.html
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from "react-helmet-async";
 import App from "./App.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
