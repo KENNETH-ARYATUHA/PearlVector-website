e39d5b44bdc3d5227616cc4b6d3b8b7811fadf6b
KENNETH-ARYATUHA <191531526+KENNETH-ARYATUHA@users.noreply.github.com>
2026-09-14 16:36:37 +0300

    Import Seo component in BlogPost

--- src/pages/BlogPost.jsx
+++ src/pages/BlogPost.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import { useParams, Link } from "react-router-dom";
 import { ArrowLeft, Calendar } from "lucide-react";
-
+import Seo from "../components/Seo";
 export default function BlogPost() {
   const { slug } = useParams();
   const [post, setPost] = useState(null);
